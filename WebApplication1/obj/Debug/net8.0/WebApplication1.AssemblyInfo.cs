8cdcf354b29cdc2925f91573233514dc17a38a0f
Makrand_D <m.v.damale@gmail.com>
2025-01-15 12:15:06 +0530

    aa

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7249bf88e3d687553444e79bb22151692764d0dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4194dfda3cbacf81d66a9d4b1accb05b16c4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
